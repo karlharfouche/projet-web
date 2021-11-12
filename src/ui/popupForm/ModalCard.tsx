74a90d723827e185d3ab45dcd99044b90f867c22
karlharfouche <karlharfouche1@gmail.com>
2021-11-12 23:00:49 +0200

    Design of posts + Add request form design awe

--- src/ui/popupForm/ModalCard.tsx
+++ src/ui/popupForm/ModalCard.tsx
@@ -9,6 +9,10 @@ import { Box, maxWidth, width } from "@mui/system";
 import { Padding } from "@mui/icons-material";
 import { TextField } from "@mui/material";
 import { Grid } from "@mui/material";
+import { FormControl } from "@mui/material";
+import { InputLabel } from "@mui/material";
+import { Input } from "@mui/material";
+import { InputAdornment } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -24,7 +28,7 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 20,
     width: '30%',
     margin: 'auto',
-    marginTop: '20%'
+    marginTop: '10%'
   }
 }));
 
@@ -50,14 +54,25 @@ export default withCardon<Props, boolean>(function ModalCard({
         <Box p={1} >
             <Grid container spacing={2} direction="column" justifyContent="center" sx={{  }}>
                 <Grid item xs={12} sx={{ mx: "auto", mt: 3 }}>
-                    <TextField required id="outlined-required" label="Title" sx={{ backgroundColor: 'white', width: 300, borderRadius: 2 }}/>
+                    <TextField required label="Title" sx={{ backgroundColor: 'white', width: 300, borderRadius: 2 }}/>
                 </Grid>
                 <Grid item xs={12} sx={{ mx: "auto" }}>
-                    <TextField id="outlined-multiline-static" label="Description" multiline rows={6} sx={{ backgroundColor: 'white', width: 300, borderRadius: 2 }}/>
+                    <TextField required label="Description" multiline rows={6} sx={{ backgroundColor: 'white', width: 300, borderRadius: 2 }}/>
                 </Grid>
                 <Grid item xs={12} sx={{ mx: "auto" }}>
-                <Button variant="contained" color="secondary" sx={{ mx: "auto", mb: 2 }}>Submit</Button>
-
+                  <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-amount" sx={{ color: 'white' }}>Fees (Optional)</InputLabel>
+                    <Input
+                      id="standard-adornment-amount"
+                      //value={values.amount}
+                      //onChange={handleChange('amount')}
+                      startAdornment={<InputAdornment position="start">&nbsp;$</InputAdornment>}
+                      sx={{ width: 300, backgroundColor: 'white', borderRadius: 2 }}
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} sx={{ mx: "auto" }}>
+                  <Button variant="contained" color="secondary" sx={{ mx: "auto", mb: 2 }}>Submit</Button>
                 </Grid>
             </Grid>
         </Box>
