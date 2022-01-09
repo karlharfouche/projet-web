d8092079d6ce3c86073e252b91707f6f089dc7be
karlharfouche <karlharfouche1@gmail.com>
2022-01-10 01:37:23 +0200

    Last commit n2alla rad

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path');
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
@@ -159,17 +158,3 @@ app.post('/api/posts/delete/userDel', async (req, res) => {
     return res.json({ status: 'ok' })
 })
 
-
-
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-});
